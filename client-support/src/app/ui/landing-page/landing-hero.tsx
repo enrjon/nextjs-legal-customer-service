8920c6a41c626b74aa5f4a013a841401e99a9bde
epinedo <epinedo@robotmonkeyllc.com>
2025-09-21 15:56:14 -0400

    updated global.css

--- client-support/src/app/ui/landing-page/landing-hero.tsx
+++ client-support/src/app/ui/landing-page/landing-hero.tsx
@@ -29,8 +29,8 @@ export default function LandingHero() {
             <div className="bg-blue-900 md:bg-transparent flex flex-col justify-center px-[16px] py-[32px] gap-[24px] max-w-[650]">
                 <div className="flex flex-col-reverse md:flex-col text-white">
                     <div className="flex flex-col ">
-                        <h1 className="text-h3 font-bold text-center">We&apos;re more than an answering service</h1>
-                        <p className="p text-b1">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
+                        <h1 className="text-h2 lg:text-h1 text-center">We&apos;re more than an answering service</h1>
+                        <p className="p text-sub2">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
                     </div>
 
                     <div className="flex flex-row justify-center gap-[16px]">
