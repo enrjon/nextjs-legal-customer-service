20bfd4652a76af29ad31ef338f6f789dc9f07b27
epinedo <epinedo@robotmonkeyllc.com>
2025-09-21 16:40:51 -0400

    updated crm styling

--- client-support/src/app/ui/landing-page/crm.tsx
+++ client-support/src/app/ui/landing-page/crm.tsx
@@ -1,41 +1,51 @@
 import Image from "next/image"
-import crmImage from '/images/CRM-Graphic/png'
 
-export default function CRM(){
+export default function CRM() {
     const crmList = [
-                            'Answering Legal’s receptionists take down exactly the information you need.',
-                            'Through native API integrations with our proprietary software, we make sure it’s immediately where you need it to be.',
-                            'All you’ll have to do is open your CRM and follow up to secure your new client’s business.',
-                        ]
+        'Answering Legal’s receptionists take down exactly the information you need.',
+        'Through native API integrations with our proprietary software, we make sure it’s immediately where you need it to be.',
+        'All you’ll have to do is open your CRM and follow up to secure your new client’s business.',
+    ]
     return (
-        <div className="flex bg-white justify-center">
-            <Image
-                src='/images/CRM-Graphic.png'
-                width={534}
-                height={453}
-                
-                alt="image of CRM messaging being utilized."
-            ></Image>
-            <div className="w-110">
-                <h2 className="text-h3 font-bold">Your data’s in good hands</h2>
-                <h4>Our partnership with your CRM enables a seamless flow of information.</h4>
-                <ul>
-                    {
-                        
-                        crmList.map((li, i)=>{
-                            return <div key={i}>
-                                <Image 
-                                    src="/images/checkmark.svg"
-                                    width={32}
-                                    height={32}
-                                    alt="Blue circle with white checkmark" />
-                                    {li}
-                                </div>
-                        })
-                    }
-                </ul>
-                <button className="btn">See our Pricing</button>
+        <div className="flex flex-col bg-white items-center justify-center gap-[50px] px-[16px] py-[32px]">
+            <div className="flex flex-col lg:flex-row gap-[32px] max-w-[1100px]">
+                <Image
+                    src='/images/CRM-Graphic.png'
+                    width={534}
+                    height={453}
+                    alt="image of CRM messaging being utilized."
+                    className="min-w-[364px] max-w-[534px]"
+                ></Image>
+                <div className="flex flex-col gap-[32px] py-[32px]">
+                    <div className="gap-[24px]">
+                        <h2 className="text-h2 font-bold">Your data’s in good hands</h2>
+                        <div className="w-full h-[2px] bg-blue-500 rounded-[15px]"></div>
+                        <h4 className="text-b2">Our partnership with your CRM enables a seamless flow of information.</h4>
+                        <ul>
+                            {
+
+                                crmList.map((li, i) => {
+                                    return <div key={i} className="flex gap-[16px]">
+                                        <Image
+                                            src="/images/checkmark.svg"
+                                            width={32}
+                                            height={32}
+                                            alt="Blue circle with white checkmark" />
+                                        <div className="text-b1">
+                                            {li}
+                                        </div>
+                                    </div>
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div>
+                        <button className="btn px-[24px] py-[11px] text-sub1 gap-[10px] rounded-[12px]">See our Pricing</button>
+                    </div>
+
+                </div>
             </div>
+
         </div>
     )
 }
