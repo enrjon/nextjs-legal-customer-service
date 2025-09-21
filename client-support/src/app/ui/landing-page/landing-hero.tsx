7d3daa1790b9025561844a86ef13489b563564be
epinedo <epinedo@robotmonkeyllc.com>
2025-09-20 22:13:55 -0400

    added style spacing for topnav and hero

--- client-support/src/app/ui/landing-page/landing-hero.tsx
+++ client-support/src/app/ui/landing-page/landing-hero.tsx
@@ -4,37 +4,46 @@ import Image from "next/image"
 
 export default function LandingHero() {
     return (
-        <div className="relative flex justify-center">
+        <div className="flex content-center flex-col md:flex-row ">
             <Image
                 src='/images/LandingHero.jpg'
                 alt='Landing Page Hero image'
                 fill
                 style={{
-                    objectFit: 'cover',
+                    objectFit: 'cover'
                 }}
-                className="-z-10 hidden md:inline-flex"
+                className="-z-10 hidden flex md:inline-flex "
             >
             </Image>
             <Image
                 src='/images/LandingHeroMobile.jpg'
                 alt='Landing Page Hero image for mobile'
-                fill
+                height={1000}
+                width={1000}
                 style={{
                     objectFit: 'cover',
                 }}
                 className="-z-10 inline-flex md:hidden"
             >
             </Image>
-            <div className="text-white py-5">
-                <h1 className="text-h2 ">We&apos;re more than an answering service</h1>
-                <p className="py-2 text-b1">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
-                <div className="flex flex-col w-80">
-                    <h3 className="text-h3 font-bold">Excellent 4.84</h3>
-                    <p className="text-h2 text-yellow-600 font-bold">★★★★★</p>
-                    <p className="text-sub2">based on 230 reviews</p>
-                    <button className="btn">See our pricing</button>
+            <div className="bg-blue-900 md:bg-transparent flex flex-col justify-center px-[16px] py-[32px] gap-[24px] max-w-[650]">
+                <div className="flex flex-col-reverse md:flex-col text-white">
+                    <div className="flex flex-col ">
+                        <h1 className="text-h3 font-bold text-center">We&apos;re more than an answering service</h1>
+                        <p className="p text-b1">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
+                    </div>
+
+                    <div className="flex flex-row justify-center gap-[16px]">
+                        <h3 className="text-h3 font-bold text-center content-center">Excellent 4.84</h3>
+                        <div className="flex flex-col text-center">
+                            <p className="text-h3 lg:text-h2 text-yellow-600 font-bold">★★★★★</p>
+                            <p className="text-sub2">based on 230 reviews</p>
+                        </div>
+                    </div>
                 </div>
+                <button className="btn rounded-[12px] py-2">See our pricing</button>
             </div>
+
         </div>
 
     )
--- client-support/src/app/ui/landing-page/topnav.tsx
+++ client-support/src/app/ui/landing-page/topnav.tsx
@@ -10,8 +10,8 @@ export default function TopNav() {
     const [collapse, setCollapse] = useState(true);
 
     return (
-        <nav className="flex flex-col justify-between bg-white px-2 py-2 relative">
-            <div className="hidden flex md:inline-flex justify-end">
+        <nav className="flex flex-col justify-between bg-white px-2 py-2">
+            <div className="hidden flex lg:inline-flex justify-end">
                 <Link href={''}>Support</Link>
                 <Link href={''}>(631) 686-9700</Link>
             </div>
@@ -27,7 +27,7 @@ export default function TopNav() {
                     alt="Answering Legal Logo"
                 ></Image>
                 <div className="flex hidden px-3 justify-between lg:inline-flex">
-                    <div className="flex pr-8 hidden md:inline-flex">
+                    <div className="flex pr-8 hidden lg:inline-flex">
                         {
                             ["Products", "About", "Resources"].map((name) => {
                                 return <div key={name} className="px-5 self-center">
@@ -40,14 +40,14 @@ export default function TopNav() {
                         </div>
                     </div>
                     <div>
-                        <div className="flex content-center justify-end gap-[9.2px]">
+                        <div className="flex content-center justify-end lg:gap-[3px] lg:gap-[9.2px]">
                             <Image
                                 width={32}
                                 height={32}
                                 src='/images/Search.svg'
                                 alt="search icon" />
                             <Link href={'/try-for-free'}
-                                className="btn text-sub1 font-bold inline-block content-center align-middle px-[20px]  py-[8px] rounded-[11.03px]">
+                                className="btn text-sub2 font-bold inline-block content-center align-middle px-[20px]  py-[8px] rounded-[11.03px]">
                                 Try for free
                             </Link>
 
@@ -56,7 +56,7 @@ export default function TopNav() {
 
                 </div>
                 <button data-collapse-toggle="mobile-menu" type="button"
-                    className=" inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                    className=" inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                     aria-controls="mobile-menu" aria-expanded="true">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
