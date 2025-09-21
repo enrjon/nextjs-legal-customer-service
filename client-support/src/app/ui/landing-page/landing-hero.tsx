90ba56daef765da9cc501428ebc91da8b8db4ed9
epinedo <epinedo@robotmonkeyllc.com>
2025-09-21 18:52:38 -0400

    updated spacing in hero text

--- client-support/src/app/ui/landing-page/landing-hero.tsx
+++ client-support/src/app/ui/landing-page/landing-hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function LandingHero() {
     return (
-        <div className="flex items-center flex-col lg:flex-row py-[32px] px-[64px]">
+        <div className="flex items-center flex-col lg:flex-row">
             <Image
                 src='/images/LandingHero.jpg'
                 alt='Landing Page Hero image'
@@ -26,19 +26,19 @@ export default function LandingHero() {
                 className="-z-10 inline-flex lg:hidden"
             >
             </Image>
-            <div className="bg-blue-900 text-white lg:bg-transparent flex flex-col justify-center gap-[24px] max-w-[1120px]">
-                <div className="flex flex-col max-w-[650px] gap-[32px]">
-                    <h1 className="text-h2 text-start font-bold lg:text-h1 text-center">We&apos;re more than an answering service</h1>
-                    <p className="p text-sub2 text-start font-semibold">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
-                    <div className="flex flex-col-reverse lg:flex-col">
-                        <div className="flex flex-col">
-                            <h3 className="text-h3 font-bold">Excellent 4.84</h3>
-                            <p className="text-h2 lg:text-h1 text-yellow-600 font-bold">★★★★★</p>
-                            <p className="text-sub2 font-semibold">based on 230 reviews</p>
+            <div className="bg-blue-900 text-white lg:bg-transparent flex flex-col justify-center gap-[24px] px-[64px] py-[32px] w-full">
+                <div className="flex flex-col gap-[32px] max-w-[650px]">
 
-                        </div>
+                    <div className=" lg:order-3 flex flex-row lg:flex-col items-center lg:items-start">
+                        <h3 className="text-h3 font-bold w-1/2 text-center lg:text-start">Excellent 4.84</h3>
+                        <p className="text-h2 lg:text-h1 text-yellow-600 font-bold w-1/2 text-center lg:text-start">★★★★★ <br />
+                        <span className="text-white text-sub2 font-semibold ">based on 230 reviews</span></p>
                     </div>
-                    <button className="btn text-sub1 rounded-[12px] gap-[32px] py-[8px] px-[24px]">See our pricing</button>
+
+                    <h1 className="lg:order-1 text-h2 text-center lg:text-start font-bold lg:text-h1">We&apos;re more than an answering service</h1>
+                    <p className="lg:order-2 text-sub2 text-start font-semibold">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
+
+                    <button className="lg:order-4 btn text-sub1 flex self-start rounded-[12px] gap-[32px] py-[8px] px-[24px]">See our pricing</button>
                 </div>
 
             </div>
