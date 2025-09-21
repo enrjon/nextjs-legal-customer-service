cabd9bc90c1ec88f0d938273992d32a174ea0b6b
epinedo <epinedo@robotmonkeyllc.com>
2025-09-21 18:14:35 -0400

    updated hero text nesting

--- client-support/src/app/ui/landing-page/landing-hero.tsx
+++ client-support/src/app/ui/landing-page/landing-hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function LandingHero() {
     return (
-        <div className="flex content-center flex-col md:flex-row">
+        <div className="flex items-center flex-col lg:flex-row py-[32px] px-[64px]">
             <Image
                 src='/images/LandingHero.jpg'
                 alt='Landing Page Hero image'
@@ -12,7 +12,7 @@ export default function LandingHero() {
                 style={{
                     objectFit: 'cover'
                 }}
-                className="-z-10 hidden flex md:inline-flex "
+                className="-z-10 hidden flex lg:inline-flex min-w-[400px] h-[750px]"
             >
             </Image>
             <Image
@@ -23,25 +23,24 @@ export default function LandingHero() {
                 style={{
                     objectFit: 'cover',
                 }}
-                className="-z-10 inline-flex md:hidden"
+                className="-z-10 inline-flex lg:hidden"
             >
             </Image>
-            <div className="bg-blue-900 md:bg-transparent flex flex-col justify-center px-[16px] py-[32px] gap-[24px] max-w-[650]">
-                <div className="flex flex-col-reverse md:flex-col text-white">
-                    <div className="flex flex-col ">
-                        <h1 className="text-h2 lg:text-h1 text-center">We&apos;re more than an answering service</h1>
-                        <p className="p text-sub2">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
-                    </div>
+            <div className="bg-blue-900 text-white lg:bg-transparent flex flex-col justify-center gap-[24px] max-w-[1120px]">
+                <div className="flex flex-col max-w-[650px] gap-[32px]">
+                    <h1 className="text-h2 text-start font-bold lg:text-h1 text-center">We&apos;re more than an answering service</h1>
+                    <p className="p text-sub2 text-start font-semibold">Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
+                    <div className="flex flex-col-reverse lg:flex-col">
+                        <div className="flex flex-col">
+                            <h3 className="text-h3 font-bold">Excellent 4.84</h3>
+                            <p className="text-h2 lg:text-h1 text-yellow-600 font-bold">★★★★★</p>
+                            <p className="text-sub2 font-semibold">based on 230 reviews</p>
 
-                    <div className="flex flex-row justify-center gap-[16px]">
-                        <h3 className="text-h3 font-bold text-center content-center">Excellent 4.84</h3>
-                        <div className="flex flex-col text-center">
-                            <p className="text-h3 lg:text-h2 text-yellow-600 font-bold">★★★★★</p>
-                            <p className="text-sub2">based on 230 reviews</p>
                         </div>
                     </div>
+                    <button className="btn text-sub1 rounded-[12px] gap-[32px] py-[8px] px-[24px]">See our pricing</button>
                 </div>
-                <button className="btn rounded-[12px] py-2">See our pricing</button>
+
             </div>
 
         </div>
