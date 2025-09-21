7dc6a2750a0eb5cfc14b349a3f9900799854819d
epinedo <epinedo@robotmonkeyllc.com>
2025-09-21 16:14:10 -0400

    updated cards reactivity

--- client-support/src/app/ui/landing-page/cards.tsx
+++ client-support/src/app/ui/landing-page/cards.tsx
@@ -20,12 +20,16 @@ export default function Cards() {
     ]
 
     return (
-        <div className="flex justify-center flex-wrap gap-[36px]">
-            <h3 className="text-h3 font-bold text-white flex">Here&apos;s what we can do for your law firm</h3>
-            <div className="flex gap-[32px]">
+        <div className="flex justify-center flex-col gap-[36px]">
+            <div className="flex flex-col gap-[24px] p-[80px] items-center">
+                <h3 className="text-h3 font-bold flex text-center md:text-white">Here{`'`}s what we can do for your law firm</h3>
+                <div className="w-full bg-blue-500 h-[2px]"></div>
+            </div>
+
+            <div className="flex gap-[32px] flex-col lg:flex-row items-center justify-center">
                 {
                     cards.map((card, i) => {
-                        return <div key={`card-${i}`} className="card px-[16px] py-[32px] justify-between">
+                        return <div key={`card-${i}`} className="card px-[16px] py-[32px] justify-between gap-[8px]">
                             <Image
                                 src={card.image}
                                 width={100}
@@ -33,9 +37,9 @@ export default function Cards() {
                                 alt={`${card.label} vector image`}
                                 className="flex"
                             ></Image>
-                            <h2 className="text-h3 text-center font-bold">{card.label}</h2>
+                            <h2 className="text-h2 text-center font-bold">{card.label}</h2>
                             <div className="w-full h-1 bg-blue-300"></div>
-                            <p>{card.description}</p>
+                            <p className="text-sub2 font-semibold">{card.description}</p>
                         </div>
                     })
                 }
