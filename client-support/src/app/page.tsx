0a5957b23aebc0b263a9150dae9d5b3e23657bbb
epinedo <epinedo@robotmonkeyllc.com>
2025-09-21 21:49:34 -0400

    added link to landing page in home

--- client-support/src/app/page.tsx
+++ client-support/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Visit http://localhost:3000/landing-page
+      <Link href='/landing-page'>To Landing Page</Link>
     </div>
   );
 }
