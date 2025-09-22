81657737ee4a7179c7bd39f66bbd0201f31e4c72
epinedo <epinedo@robotmonkeyllc.com>
2025-09-22 01:04:20 -0400

    updated colors for intake

--- client-support/src/app/ui/landing-page/intake.tsx
+++ client-support/src/app/ui/landing-page/intake.tsx
@@ -4,8 +4,8 @@ export default function Intake() {
     return (
         <div className="flex justify-center gap-[50px] p-[32px] lg:py-[64]">
             <div className="flex flex-col-reverse items-center md:flex-row gap-[32px]">
-                <div className="flex flex-col gap-[32px] max-w-[534px]">
-                    <div className="gap-[32px] py-[32px]">
+                <div className="flex flex-col max-w-[534px]">
+                    <div className="flex flex-col gap-[32px] py-[32px]">
                         <h3 className="text-h2 font-bold">Design your perfect legal intake process</h3>
                         <div className="w-full h-[2px] divider"></div>
                         <p className="text-b2">Get everything you need out of every new client call. The virtual receptionists at our legal intake call center will use your unique specifications to perform a legal intake for every new client caller.</p>
