e79a03ac2cb3be41a979b81e9f7a119d8545a548
epinedo <epinedo@robotmonkeyllc.com>
2025-09-21 20:07:14 -0400

    updated form styling

--- client-support/src/app/ui/landing-page/Form.tsx
+++ client-support/src/app/ui/landing-page/Form.tsx
@@ -34,26 +34,32 @@ export default function Form() {
     ];
 
     return (
-        <div className="flex flex-col lg:flex-row px-[16px] py-[64px] gap-[64px]" style={{ backgroundColor: "#161641" }}>
-            <div className="text-white ">
-                <h1 className="text-h2">24/7 coverage at a fraction of the cost of in-house receptionists.</h1>
-                <p>Don’t believe us? Fill out the form on the right, and we{`'`}ll take you to our pricing page, where you can find out just how little 24/7 legal intake will cost you.</p>
+        <div className="flex flex-col px-[16px] py-[64px] gap-[64px]" style={{ backgroundColor: "#161641" }}>
+            <div className="flex flex-col lg:flex-row gap-[64px] self-center max-w-[1120px]">
+                <div className="text-white flex flex-col justify-center gap-[10px] min-w-[400px]">
+                    <div className="flex flex-col justify-center gap-[32px]">
+                        <h1 className="text-h2 font-bold">24/7 coverage at a fraction of the cost of in-house receptionists.</h1>
+                        <p className="text-sub1">Don’t believe us? Fill out the form on the right, and we{`'`}ll take you to our pricing page, where you can find out just how little 24/7 legal intake will cost you.</p>
+                    </div>
+                </div>
+                <form onSubmit={() => { console.log('Form submitted') }}
+                    className="flex flex-col form font-bold self-center"
+                >
+                    <h3 className="text-sub1 font-bold">Tell us about yourself. We{`'`}ll show you all of our pricing information on the next page.</h3>
+                    {
+                        formElems.map((elem) => {
+                            return (
+                                <div key={`form-${elem.id}`} className="flex flex-col">
+                                    <label>{elem.label}</label>
+                                    <input type="text" name={elem.name} id={elem.id} placeholder={elem.label} />
+                                </div>
+                            )
+                        })
+                    }
+                    <button className="btn flex self-center rounded-[12px] text-h3 p-[16px]">See plans & pricing</button>
+                </form>
             </div>
-            <form onSubmit={() => { console.log('Form submitted') }}
-                className="flex flex-col form font-bold"
-            >
-                <h3 className="text-b1 font-bold">Tell us about yourself. We{`'`}ll show you all of our pricing information on the next page.</h3>
-                {
-                    formElems.map((elem)=>{
-                        return (
-                            <div key={`form-${elem.id}`} className="flex flex-col">
-                                <label>{elem.label}</label>
-                                <input type="text" name={elem.name} id={elem.id} placeholder={elem.label}/>
-                            </div>
-                        )
-                    })
-                }
-            </form>
+
         </div>
     )
 }
